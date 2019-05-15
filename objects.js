e9faaaaca8ceef9b292592e368d562024a17c06d
Heather Arbuckle <bucklehead@gmail.com>
2019-05-15 19:15:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
  };
 
  function updatePlaylist(playlist, artistName, songTitle) {
-   Object.assign({playlist}, artistName : songTitle);
+   let playlist = Object.assign({}, {artistName : songTitle});
    return playlist;
  }
 
