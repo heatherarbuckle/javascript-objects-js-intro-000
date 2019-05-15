b13d4b1c54746a8ec480e59f1aeee74532a6bbd4
Heather Arbuckle <bucklehead@gmail.com>
2019-05-15 18:59:06 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  pink: "raise your glass",
+  tswift: "me",
+  oasis: "wonderwall"
+ };
+
+ function updatePlaylist(playlist, artist, song) {
+   object.assign({playlist}, artist: song)};
+   return playlist;
+ }
